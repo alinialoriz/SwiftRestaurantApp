be0b5171972734dbfd11036f17d6cba464335678
Alinia <alinialoriz@gmail.com>
2019-10-18 17:54:21 +1100

    Implement home navigation controls

--- RestaurantApp/ViewController.swift
+++ RestaurantApp/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var user : String = ""
 
+    @IBOutlet weak var userSelector: UISegmentedControl!
+    
+    @IBAction func signInBtn(_ sender: Any) {
+        
+        if self.userSelector.selectedSegmentIndex == 0 {
+            user = "Wait Staff"
+            performSegue(withIdentifier: "waitStaffSegue", sender: nil)
+        } else {
+            user = "Admin"
+            performSegue(withIdentifier: "adminSegue", sender: nil)
+        }
+        
+        print(user)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
