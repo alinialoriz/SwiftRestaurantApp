0b7311e73ca40f0fa27445929b73bd3776a7fd4c
Alinia <alinialoriz@gmail.com>
2019-10-19 21:27:40 +1100

    Starting view controller for add new dish view

---
+++ RestaurantApp/AddNewDishViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddNewDishViewController.swift
+//  RestaurantApp
+//
+//  Created by Alinia Mabatid on 19/10/2019.
+//  Copyright © 2019 UWS. All rights reserved.
+//
+
+import UIKit
+
+class AddNewDishViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
