3354bb08fc1162b6030b118e489b9cf26dc8adcd
Alinia <alinialoriz@gmail.com>
2019-10-20 20:39:16 +1100

    Populating ordered dishes array

--- RestaurantApp/OrderScreenViewController.swift
+++ RestaurantApp/OrderScreenViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class OrderScreenViewController: UIViewController {
-    
+class OrderScreenViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+ 
+    @IBOutlet weak var orderSummary: UITableView!
     @IBOutlet weak var staffID: UILabel!
     
     // Create a variable that holds a reference to AdminDishesTableView
@@ -18,11 +19,35 @@ class OrderScreenViewController: UIViewController {
     // Create variable to accept values passed from AdminDishesTableView
     var selectedID : Int = 0
     
+     // Create an array that contains the ordered Dish objects
+    var orderedDishes : [Dish] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set staffID input to the selectedID from WaitStaffIDViewController
         staffID.text = String(selectedID)
+        
+        // Create 3 Dish objects: dish1, dish2, dish3
+        let dish1 = Dish(image: UIImage(named: "potstickers")!, name: "Potstickers", category: "Entrée", qty: 0, price: 6.00, isSelected: true)
+        let dish2 = Dish(image: UIImage(named: "xiao-long-bao")!, name: "Xiao long bao", category: "Main", qty: 0, price: 8.00, isSelected: true)
+        let dish3 = Dish(image: UIImage(named: "spring-rolls")!, name: "Spring rolls", category: "Main", qty: 0, price: 6.00, isSelected: true)
+        
+        // Add Dish objects into the temporaryDishes array
+        orderedDishes.append(dish1)
+        orderedDishes.append(dish2)
+        orderedDishes.append(dish3)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return orderedDishes.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Populate a cell with the values of the current dish object
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dishCell", for: indexPath) as! DishTableViewCell
     }
 
 }
---
+++ RestaurantApp/OrderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderTableViewCell.swift
+//  RestaurantApp
+//
+//  Created by Alinia Mabatid on 20/10/2019.
+//  Copyright © 2019 UWS. All rights reserved.
+//
+
+import UIKit
+
+class OrderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
